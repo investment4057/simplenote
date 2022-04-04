567bebe9df8126ad05cadbff6d8d10a567cf4bd6
investment4057 <investment4057@gmail.com>
2022-04-05 00:16:33 +0900

    メモ一覧の取得

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,12 +24,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // ログインユーザーの情報をviewに渡す
+        $user = \Auth::user();
+
+        // メモ一覧を取得
+        // ASC=昇順 DESC=降順（新しい順）
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        // dd($memos);
+        return view('home', compact('user', 'memos'));
     }
 
     public function create()
     {
-        // ログインユーザーのidを渡す
+        // ログインユーザーの情報をviewに渡す
         $user = \Auth::user();
         return view('create', compact('user'));
     }
