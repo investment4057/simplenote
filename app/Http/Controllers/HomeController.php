1928fb9960481367158704fc35c088c40e8b1bb9
investment4057 <investment4057@gmail.com>
2022-04-05 12:37:06 +0900

    ViewComposer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,22 +25,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // ログインユーザーの情報をviewに渡す
-        $user = \Auth::user();
-
-        // メモ一覧を取得
-        // ASC=昇順 DESC=降順（新しい順）
-        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
-        // dd($memos);
-        return view('create', compact('user', 'memos'));
+        return view('create');
     }
 
     public function create()
     {
-        // ログインユーザーの情報をviewに渡す
-        $user = \Auth::user();
-        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
-        return view('create', compact('user', 'memos'));
+        return view('create');
     }
 
     public function store(Request $request)
@@ -76,16 +66,9 @@ class HomeController extends Controller
 
     public function edit($id)
     {
-        // 該当するIDのメモをデータベースから取得
         $user = \Auth::user();
         $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])->first();
-        // dd($memo);
-
-        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
-
-        //取得したメモをViewに渡す
-        $tags = Tag::where('user_id', $user['id'])->get();
-        return view('edit', compact('memo', 'user', 'memos', 'tags'));
+        return view('edit', compact('memo'));
     }
 
     public function update(Request $request, $id)
